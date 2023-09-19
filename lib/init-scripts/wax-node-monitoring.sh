9bdeb3eb565085aff6dcebbca25ebb1eea7380ba
Bharat Bala <bbharat95@gmail.com>
2023-09-20 04:25:48 +0530

    fixes

--- lib/init-scripts/wax-node-monitoring.sh
+++ lib/init-scripts/wax-node-monitoring.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 apiHost="localhost"
-timestamp=`date +%s`
-response=$(curl -s $apiHost:8888/v1/chain/get_info)
+statsdHost="localhost"
+statsdPort="8125"  # Replace with your StatsD port
+timestamp=$(date +%s)
+response=$(curl -s "$apiHost:8888/v1/chain/get_info")
+
 if [ $? -eq 0 ]; then
-  head_block_time=`curl -s $apiHost:8888/v1/chain/get_info | jq -r .head_block_time`
-  head_block_num=`curl -s $apiHost:8888/v1/chain/get_info | jq -r .head_block_num`
-  head_block_timestamp=`date -d "$head_block_time" +%s`
-  difference_in_time=`bc <<< "$timestamp - $head_block_timestamp"`
+  head_block_time=$(curl -s "$apiHost:8888/v1/chain/get_info" | jq -r .head_block_time)
+  head_block_num=$(curl -s "$apiHost:8888/v1/chain/get_info" | jq -r .head_block_num)
+  head_block_timestamp=$(date -d "$head_block_time" +%s)
+  difference_in_time=$(bc <<< "$timestamp - $head_block_timestamp")
 else
   head_block_time=0
   head_block_num=0
   difference_in_time=0
 fi
-echo "head_block_num value=$head_block_num $timestamp"
-echo "head_block_time value=$head_block_timestamp $timestamp"
-echo "head_block_time_difference value=$difference_in_time $timestamp"
+
+# Send metrics to StatsD
+echo "head_block_num:$head_block_num|g" | nc -u -w0 "$statsdHost" "$statsdPort"
+echo "head_block_time:$head_block_timestamp|g" | nc -u -w0 "$statsdHost" "$statsdPort"
+echo "head_block_time_difference:$difference_in_time|g" | nc -u -w0 "$statsdHost" "$statsdPort"
