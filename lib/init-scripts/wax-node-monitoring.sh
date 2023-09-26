5102962d7580b5240017a1e56bcdd2d93ba70709
Bharat Bala <bbharat95@gmail.com>
2023-09-26 16:19:19 +0530

    wax-node-grafana

--- lib/init-scripts/wax-node-monitoring.sh
+++ lib/init-scripts/wax-node-monitoring.sh
@@ -10,13 +10,16 @@ if [ $? -eq 0 ]; then
   head_block_num=$(curl -s "$apiHost:8888/v1/chain/get_info" | jq -r .head_block_num)
   head_block_timestamp=$(date -d "$head_block_time" +%s)
   difference_in_time=$(bc <<< "$timestamp - $head_block_timestamp")
+  head_block_num_global=$(bc <<< "$difference_in_time * 2")
 else
   head_block_time=0
   head_block_num=0
   difference_in_time=0
+  head_block_num_global=0
 fi
 
 # Send metrics to StatsD
 echo "head_block_num:$head_block_num|g" | nc -u -w0 "$statsdHost" "$statsdPort"
+echo "head_block_num_global:$head_block_num_global|g" | nc -u -w0 "$statsdHost" "$statsdPort"
 echo "head_block_time:$head_block_timestamp|g" | nc -u -w0 "$statsdHost" "$statsdPort"
 echo "head_block_time_difference:$difference_in_time|g" | nc -u -w0 "$statsdHost" "$statsdPort"
